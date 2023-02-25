ad1087b129a8335ef7a2532694e9c90192022171
Tolkinxon <krosstony639@gmail.com>
2023-02-26 00:57:28 +0500

    cube

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-'use strict'
-
 const button = document.querySelector('.btn')
-const top = document.querySelector('.top')
-const bottom = document.querySelector('.bottom')
-const left = document.querySelector('.left')
-const right = document.querySelector('.right')
+const top1 = document.querySelector('.top')
+const bottom1 = document.querySelector('.bottom')
+const left1 = document.querySelector('.left')
+const right1 = document.querySelector('.right')
 
-right.style.backgroundColor = 'black'
+let i = 0
 
 button.addEventListener('click', () => {
-  right.style.backgroundColor = 'black'
+  //   top1.style.cssText = `
+  //       transform:rotateX(80deg) translateZ(100px) rotateZ(${i += 10}deg);
+  //     `
+
+  right1.style.cssText = `
+        transform: rotateY(80deg) translateZ(100px) rotate3d(1, 1, 4, ${(i += 40)}deg)  ;
+        `
 })
