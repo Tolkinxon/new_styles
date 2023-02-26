648acd7c34e5fd8dcaa8baed16ef59b706ea1b04
Tolkinxon <krosstony639@gmail.com>
2023-02-26 11:16:33 +0500

    cube

--- script.js
+++ script.js
@@ -5,42 +5,38 @@ const right1 = document.querySelector('.right')
 const front1 = document.querySelector('.front')
 const behind1 = document.querySelector('.behind')
 const container = document.querySelector('.container')
-const box = document.querySelector('.box')
 
-let i = 90
-let j = 0
+let i = -90
 
-const rotateXY = (number, x, y) => {
+const rotateXY = (number, x, y, z) => {
   top1.style.cssText = `
-    transform:  rotate3d(${x}, ${y}, 0, ${(i += number)}deg) translateZ(100px); `
+    transform:  rotate3d(${x}, ${y}, ${z}, ${(i += number)}deg) translateZ(100px); `
   bottom1.style.cssText = `
-    transform:  rotate3d(${x}, ${y}, 0, ${i + 180}deg)   translateZ(100px);`
+    transform:  rotate3d(${x}, ${y},  ${z}, ${i + 180}deg)   translateZ(100px);`
   right1.style.cssText = `
-    transform: rotate3d(${x}, ${y}, 0, ${i}deg)  translateZ(100px);`
+    transform: rotate3d(${x}, ${y},  ${z}, ${i}deg)  translateZ(100px);`
   left1.style.cssText = `
-    transform: rotate3d(${x}, ${y}, 0, ${i}deg) translateZ(100px);`
+    transform: rotate3d(${x}, ${y},  ${z}, ${i}deg) translateZ(100px);`
   front1.style.cssText = `
-    transform:  rotate3d(${x}, ${y}, 0, ${i + 90}deg)   translateZ(100px);`
+    transform:  rotate3d(${x}, ${y},  ${z}, ${i + 90}deg)   translateZ(100px);`
   behind1.style.cssText = `
-    transform:  rotate3d(${x}, ${y}, 0, ${i + 270}deg)   translateZ(100px); `
+    transform:  rotate3d(${x}, ${y},  ${z}, ${i + 270}deg)   translateZ(100px); `
 }
 
 document.addEventListener('keydown', (e) => {
-  console.log(e.code)
-
   if (e.code === 'ArrowUp') {
-    rotateXY(10, 1, 0)
+    rotateXY(10, 1, 0, 0)
   }
 
   if (e.code === 'ArrowDown') {
-    rotateXY(-10, 1, 0)
+    rotateXY(-10, 1, 0, 0)
   }
 
   if (e.code === 'ArrowRight') {
-    rotateXY(10, 0, 1)
+    rotateXY(10, 0, 1, 0)
   }
 
   if (e.code === 'ArrowLeft') {
-    rotateXY(-10, 0, 1)
+    rotateXY(-10, 0, 1, 0)
   }
 })
