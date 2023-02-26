dc0acdec620041845d8e77be4f766d45f7a616fe
Tolkinxon <krosstony639@gmail.com>
2023-02-26 07:09:50 +0500

    cube

--- script.js
+++ script.js
@@ -1,41 +1,47 @@
-const button = document.querySelector('.btn')
 const top1 = document.querySelector('.top')
 const bottom1 = document.querySelector('.bottom')
 const left1 = document.querySelector('.left')
 const right1 = document.querySelector('.right')
 const front1 = document.querySelector('.front')
 const behind1 = document.querySelector('.behind')
+const container = document.querySelector('.container')
 
-let t = 0
-let b = 0
-let r = 0
-let l = 0
-let fr = 90
-let bh = 90
+let i = 0
 
-button.addEventListener('click', () => {
-  top1.style.cssText = `
-        transform:rotateX(90deg) translateZ(100px) rotateZ(${(t += 10)}deg);
-      `
-  bottom1.style.cssText = `
-      transform:rotateX(90deg) translateZ(-100px) rotateZ(${(b += 10)}deg);
-    `
+document.addEventListener('keydown', (e) => {
+  console.log(e.code)
 
-  right1.style.cssText = `
-            transform-origin: center top -100px;
-          transform:translateZ(100px) rotateY(${(r -= 10)}deg)  ;
+  // if (e.code === 'ArrowUp') {
+  //   top1.style.cssText = `
+  //           transform: rotateX(90deg) rotateX(${(i += 10)}deg)   translateZ(100px);
+  //          `
+  //   bottom1.style.cssText = `
+  //          transform: rotateX(90deg) rotateX(${((i += 10) + 180)}deg)   translateZ(100px);
+  //     `
+  // }
+  // if (e.code === 'ArrowDown') {
+  //   top1.style.cssText = `
+  //               transform: rotateX(90deg) rotateX(${(i -= 10)}deg)   translateZ(100px);
+  //          `
+  //   bottom1.style.cssText = `
+  //          transform: rotateX(90deg) rotateX(${((i -= 10) + 180)}deg)   translateZ(100px);
+  //     `
+  // }
+
+  if (e.code === 'ArrowUp') {
+    top1.style.cssText = `
+            transform:  rotateX(${(i += 10)}deg)   translateZ(100px);  
+           `
+    bottom1.style.cssText = `
+           transform:  rotateX(${(i += 10) + 180}deg)   translateZ(100px);  
           `
-  left1.style.cssText = `
-          transform-origin: center top 100px;
-          transform: translateZ(-100px) rotateY(${(l -= 10)}deg)  ;
+  }
+  if (e.code === 'ArrowDown') {
+    top1.style.cssText = `
+                transform: rotateX(${(i -= 10)}deg)   translateZ(100px);  
+           `
+    bottom1.style.cssText = `
+           transform:  rotateX(${(i -= 10) + 180}deg)   translateZ(100px);  
           `
-
-  front1.style.cssText = `
-          transform-origin: center top -100px;
-          transform:translateZ(100px) rotateY(${(fr -= 10)}deg)  ;
-          `
-  behind1.style.cssText = `
-          transform-origin: center top 100px;
-          transform: translateZ(-100px) rotateY(${(bh -= 10)}deg)  ;
-  `
+  }
 })
