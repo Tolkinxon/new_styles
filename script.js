a7edcb8057e1a9fd4d920500cfa3b08f0c79308f
Tolkinxon <krosstony639@gmail.com>
2023-02-26 13:36:47 +0500

    cube

--- script.js
+++ script.js
@@ -7,36 +7,73 @@ const behind1 = document.querySelector('.behind')
 const container = document.querySelector('.container')
 
 let i = -90
+let j = 0
 
-const rotateXY = (number, x, y, z) => {
+// const rotateXY = (number, x, y, z) => {
+//   top1.style.cssText = `
+//     transform:  rotate3d(${x}, ${y}, ${z}, ${(i += number)}deg) translateZ(100px); `
+//   bottom1.style.cssText = `
+//     transform:  rotate3d(${x}, ${y},  ${z}, ${i + 180}deg)   translateZ(100px);`
+//   right1.style.cssText = `
+//     transform: rotate3d(${x}, ${y},  ${z}, ${i}deg)  translateZ(100px);`
+//   left1.style.cssText = `
+//     transform: rotate3d(${x}, ${y},  ${z}, ${i}deg) translateZ(100px);`
+//   front1.style.cssText = `
+//     transform:  rotate3d(${x}, ${y},  ${z}, ${i + 90}deg)   translateZ(100px);`
+//   behind1.style.cssText = `
+//     transform:  rotate3d(${x}, ${y},  ${z}, ${
+//     i + 270
+//   }deg)   translateZ(100px); `
+// }
+
+const rotateX = (number) => {
   top1.style.cssText = `
-    transform:  rotate3d(${x}, ${y}, ${z}, ${(i += number)}deg) translateZ(100px); `
+    transform: rotateY(${j}deg)  rotateX(${(i += number)}deg) translateZ(100px); `
   bottom1.style.cssText = `
-    transform:  rotate3d(${x}, ${y},  ${z}, ${i + 180}deg)   translateZ(100px);`
+    transform: rotateY(${j}deg)  rotateX(${i + 180}deg)   translateZ(100px);`
   right1.style.cssText = `
-    transform: rotate3d(${x}, ${y},  ${z}, ${i}deg)  translateZ(100px);`
+    transform: rotateY(${j}deg) rotateX(${i}deg)  translateZ(100px);`
   left1.style.cssText = `
-    transform: rotate3d(${x}, ${y},  ${z}, ${i}deg) translateZ(100px);`
+    transform: rotateY(${j}deg) rotateX(${i}deg) translateZ(100px);`
   front1.style.cssText = `
-    transform:  rotate3d(${x}, ${y},  ${z}, ${i + 90}deg)   translateZ(100px);`
+    transform:  rotateY(${j}deg)  rotateX(${
+    i + 90
+  }deg)   translateZ(100px);`
   behind1.style.cssText = `
-    transform:  rotate3d(${x}, ${y},  ${z}, ${i + 270}deg)   translateZ(100px); `
+    transform: rotateY(${j}deg)  rotateX(${
+    i + 270
+  }deg)  translateZ(100px); `
+}
+
+const rotateY = (number) => {
+  top1.style.cssText = `
+  transform: rotateX(${i}deg) rotateY(${(j += number)}deg)  translateZ(100px);`
+  right1.style.cssText = `
+  transform:  rotateX(${i}deg)   rotateY(${j}deg) translateZ(100px); `
+  left1.style.cssText = `
+  transform: rotateX(${i}deg)   rotateY(${j + 180}deg)   translateZ(100px);`
+  bottom1.style.cssText = `
+  transform: rotateX(${i}deg)  rotateY(${j + 90}deg) translateZ(100px);`
+  front1.style.cssText = `
+  transform: rotateX(${i}deg)   rotateY(${j}deg)   translateZ(100px);`
+  behind1.style.cssText = `
+  transform: rotateX(${i}deg) rotateY(${j + 270}deg)  translateZ(100px); `
 }
 
 document.addEventListener('keydown', (e) => {
   if (e.code === 'ArrowUp') {
-    rotateXY(10, 1, 0, 0)
+    rotateX(10)
   }
 
   if (e.code === 'ArrowDown') {
-    rotateXY(-10, 1, 0, 0)
+    rotateX(-10)
   }
 
   if (e.code === 'ArrowRight') {
-    rotateXY(10, 0, 1, 0)
+    rotateY(10)
   }
 
   if (e.code === 'ArrowLeft') {
-    rotateXY(-10, 0, 1, 0)
+    rotateY(-10)
   }
 })
